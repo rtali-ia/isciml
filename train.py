f433a08d7390187eca3931601ceb931bdffa390f
Santi Adavani <santis@gmail.com>
2023-05-11 07:21:30 -0700

    training starter script

---
+++ train.py
@@ -0,0 +1,47 @@
+import lightning.pytorch as pl
+from torch.utils.data import Dataset
+
+class LitAutoEncoder(pl.LightningModule):
+    def __init__(self,encoder,decoder):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def training_step(self, batch, batch_idx):
+        # training_step defines the train loop.
+        # it is independent of forward
+        x, y = batch
+        x = x.view(x.size(0), -1)
+        z = self.encoder(x)
+        x_hat = self.decoder(z)
+        loss = nn.functional.mse_loss(x_hat, x)
+        # Logging to TensorBoard (if installed) by default
+        self.log("train_loss", loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+
+class NumpyDataset(Dataset):
+    def __init__(self, sample_dir, target_dir, transform=None, target_transform=None):
+        self.sample_dir = sample_dir
+        self.target_dir = target_dir
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+        return len(self.img_labels)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        image = read_image(img_path)
+        label = self.img_labels.iloc[idx, 1]
+        if self.transform:
+            image = self.transform(image)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return image, label
+
+
