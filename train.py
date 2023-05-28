9e33734516395c54ceaad3e18ec22d18c6f49af6
Santi Adavani <santis@gmail.com>
2023-05-28 15:16:02 -0700

    Training first version

--- train.py
+++ train.py
@@ -1,47 +1,61 @@
 import lightning.pytorch as pl
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
+import torch
+import model
+import numpy as np
+import glob, os
+
 
 class LitAutoEncoder(pl.LightningModule):
-    def __init__(self,encoder,decoder):
+    def __init__(self):
         super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
+        self.unet = model.UNet(out_channels=1)
 
     def training_step(self, batch, batch_idx):
         # training_step defines the train loop.
         # it is independent of forward
         x, y = batch
-        x = x.view(x.size(0), -1)
-        z = self.encoder(x)
-        x_hat = self.decoder(z)
-        loss = nn.functional.mse_loss(x_hat, x)
+        x_hat = self.unet(x)
+        loss = F.mse_loss(x_hat, y)
         # Logging to TensorBoard (if installed) by default
         self.log("train_loss", loss)
         return loss
 
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
         return optimizer
 
 
 class NumpyDataset(Dataset):
-    def __init__(self, sample_dir, target_dir, transform=None, target_transform=None):
+    def __init__(self, sample_dir, target_dir):
         self.sample_dir = sample_dir
         self.target_dir = target_dir
-        self.transform = transform
-        self.target_transform = target_transform
+
+        self.sample_files = []
+        for fname in glob.iglob(self.sample_dir + "/**", recursive=True):
+            if os.path.isfile(fname) and fname.endswith(".npy"):
+                self.sample_files.append(fname)
+
+        self.target_files = []
+        for fname in glob.iglob(self.target_dir + "/**", recursive=True):
+            if os.path.isfile(fname) and fname.endswith(".npy"):
+                self.target_files.append(fname)
+
+        assert len(self.sample_files) == len(self.target_files)
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.sample_files)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
-        image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
-        if self.transform:
-            image = self.transform(image)
-        if self.target_transform:
-            label = self.target_transform(label)
-        return image, label
+        sample = np.load(self.sample_files[idx]).astype(np.float32)
+        target = np.load(self.target_files[idx]).astype(np.float32)
+        return sample, target
 
 
+npydataset = NumpyDataset("/tmp/samples/", "/tmp/targets")
+dataloader = DataLoader(npydataset, batch_size=8)
+
+model = LitAutoEncoder()
+trainer = pl.Trainer()
+trainer.fit(model=model, train_dataloaders=dataloader)
